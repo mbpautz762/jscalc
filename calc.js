b21427c765f97075e275b9eb136b5a32a5feb2ba
mbpautz762 <mbpautz762@gmail.com>
2022-05-20 12:44:24 -0400

    basic shape and layout

--- calc.js
+++ calc.js
@@ -1,1 +1,21 @@
 // js functionality for calculator
+let displayResults = document.querySelector("#display .results");
+displayResults.textContent = "Results Display Here";
+
+let displayInput = document.querySelector("#display .input");
+displayInput.textContent = "User Input Here";
+
+let buttons = createButtons();
+assignButtonText(buttons);
+
+
+function createButtons() {
+    let list = document.querySelectorAll(".button");
+    return Array.from(list);
+}
+
+function assignButtonText(buttons) {
+    buttons.forEach(element => {
+        console.log(element.id);
+    });
+}
