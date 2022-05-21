e3c79fce924200e414f6c1be2293790b09f9ebb7
mbpautz762 <mbpautz762@gmail.com>
2022-05-20 21:19:31 -0400

    added basic functionality, no logic

--- calc.js
+++ calc.js
@@ -1,21 +1,42 @@
 // js functionality for calculator
-let displayResults = document.querySelector("#display .results");
-displayResults.textContent = "Results Display Here";
+let results = document.querySelector("#display .results");
+results.textContent = "";
 
-let displayInput = document.querySelector("#display .input");
-displayInput.textContent = "User Input Here";
+let input = document.querySelector("#display .input");
+input.textContent = "";
 
 let buttons = createButtons();
-assignButtonText(buttons);
-
 
 function createButtons() {
     let list = document.querySelectorAll(".button");
-    return Array.from(list);
-}
-
-function assignButtonText(buttons) {
+    const buttons = Array.from(list);
     buttons.forEach(element => {
         element.textContent = `${element.id}`;
+        element.addEventListener("click", (e) => {
+            if (e.target.id === "enter") {
+                results.textContent = "";
+                results.textContent = evalExpression(input.textContent);
+            }
+            else if (e.target.id === "c") {input.textContent = "";}
+            else if (e.target.id === "ce") {
+                input.textContent = "";
+                results.textContent = "";
+            }
+            else input.textContent = buildExpression(input.textContent, e);
+        });
     });
 }
+
+function buildExpression(input, e) {
+    // if (e.target.id === "enter") return input;
+    if (e.target.id === "del") return input.slice(0, -1);
+    
+    return input.concat(e.target.id);
+}
+
+
+function evalExpression(input) {
+    return input.concat(" = (evaluated)");
+
+}
+
