9cfef4142bda4ccbee2bb7c6276e73b487c46044
mbpautz762 <mbpautz762@gmail.com>
2022-05-21 16:18:09 -0400

    added more calc functionality

--- calc.js
+++ calc.js
@@ -3,9 +3,17 @@ let results = document.querySelector("#display .results");
 results.textContent = "";
 
 let input = document.querySelector("#display .input");
-input.textContent = "";
+input.textContent = "0";
 
 let buttons = createButtons();
+let expressionObj = 
+    {"l_operand" : "",
+    "operator" : "",
+    "r_operand" : "",
+    "active" : "none",
+    "result" : "",
+    "fraction" : false};
+
 
 function createButtons() {
     let list = document.querySelectorAll(".button");
@@ -13,38 +21,132 @@ function createButtons() {
     buttons.forEach(element => {
         element.textContent = `${element.id}`;
         element.addEventListener("click", (e) => {
-            if (e.target.id === "enter") {
-                results.textContent = "";
-                results.textContent = evalExpression(input.textContent);
+            if (e.target.classList.contains("numeric")) {
+                // if there is no current left and/or operand, store it
+                if (expressionObj.operator === "") {
+                    expressionObj.l_operand += e.target.id;
+                    expressionObj.active = "l";
+                }
+                else if (expressionObj.operator !== "") {
+                    expressionObj.r_operand += e.target.id;
+                    expressionObj.active = "r";
+                }
             }
-            else if (e.target.id === "c") {input.textContent = "";}
-            else if (e.target.id === "ce") {
-                input.textContent = "";
-                results.textContent = "";
+            if (e.target.classList.contains("operator")) {
+                if (expressionObj.operator === "") {
+                    expressionObj.operator = e.target.id;
+                }
+                else {
+                    // if user inputs new operator and there is already a right operand,
+                    // auto evaluate the expression and reset expression w/ result stored in l_operand
+                    if (expressionObj.r_operand !== "") {
+                        evaluateExpression();
+                        expressionObj.l_operand = expressionObj.result;
+                        expressionObj.operator = e.target.id;
+                    }
+                }
+                expressionObj.active = "o";
             }
-            else input.textContent = buildExpression(input.textContent, e);
+            if (e.target.classList.contains("special")) {
+                switch (e.target.id) {
+                    case "c":
+                        resetExpression();
+                        break;
+                    case "ce":
+                        resetExpression();
+                        expressionObj.result = "";
+                        break;
+                    case "del":
+                        deleteLast();
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+            // now set the proper display(s)
+            input.textContent = expressionObj.l_operand + " " + expressionObj.operator + " " + expressionObj.r_operand;
+            results.textContent = expressionObj.result;
+            console.log(expressionObj);
         });
     });
 }
 
-function buildExpression(input, e) {
-    // if (e.target.id === "enter") return input;
-    if (e.target.id === "del") return input.slice(0, -1);
+function evaluateExpression() {
+    // first, convert to numbers
+    let left = parseInt(expressionObj.l_operand);
+    let right = parseInt(expressionObj.r_operand);
+    switch (expressionObj.operator) {
+        case "+":
+            expressionObj.result = left + right;
+            break;
+        case "-":
+            expressionObj.result = left - right;
+            break;
+        case "*":
+            expressionObj.result = left * right;
+            break;
+        case "/":
+            if (right === 0) expressionObj.result = "Don't div by 0!";
+            else expressionObj.result = left / right;
     
-    return input.concat(e.target.id);
+        default:
+            break;
+    }
+    results.textContent = expressionObj.result;
+    resetExpression();
 }
 
+function resetExpression() {
+    expressionObj.l_operand = "";
+    expressionObj.r_operand = "";
+    expressionObj.operator = "";
+    expressionObj.active = "none";
+}
+// this is clunky, but it only deletes from what was inputted last, without having 
+// to build the entire expression into a string
+function deleteLast() {
+    switch (expressionObj.active) {
+        case "none":
+            return;
+            break;
+        case "l":
+            expressionObj.l_operand = expressionObj.l_operand.slice(0, -1);
+            break;
+        case "o":
+            expressionObj.operator = "";
+            expressionObj.active = "l";
+            break;
+        case "r":
+            expressionObj.r_operand = expressionObj.r_operand.slice(0, -1);
+            if (expressionObj.l_operand == "") expressionObj.active = "o";
 
-function evalExpression(input) {
-const operands = input.split(/([*+-/])/);
-
-for (let i = 0; i < operands.length; i++) {
-    if (!(/[+*-/]/.test(operands[i]))) {
-        operands[i] = parseInt(operands[i]);
+            break;        
+    
+        default:
+            break;
     }
 }
-    console.log(operands);
-    return input.concat(" = (evaluated)");
 
-}
+
+// function buildExpression(input, e) {
+//     // if (e.target.id === "enter") return input;
+//     if (e.target.id === "del") return input.slice(0, -1);
+    
+//     return input.concat(e.target.id);
+// }
+
+
+// function evalExpression(input) {
+// const operands = input.split(/([*+-/])/);
+
+// for (let i = 0; i < operands.length; i++) {
+//     if (!(/[+*-/]/.test(operands[i]))) {
+//         operands[i] = parseInt(operands[i]);
+//     }
+// }
+//     console.log(operands);
+//     return input.concat(" = (evaluated)");
+
+// }
 
